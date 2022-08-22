68616ce5a6b69ec06be1853e6d855ece09922424
Raison Robert Monteiro de Sá <raison.robert@pucpr.edu.br>
2022-08-22 12:01:22 -0400

    Init server

---
+++ src/main/kotlin/com/pucpr/helloSpring/Exercicio_01.kt
@@ -0,0 +1,13 @@
+package com.pucpr.helloSpring
+//
+//Exercico1:
+//
+//Faça um form/serviço onde o usuário possa colocar duas datas (data inicio e data fim) e o servidor retorne;
+//
+//
+//
+//A diferença de dias entre as datas;
+//
+//A diferença de semana entre as datas;
+//
+//A diferença de messes entre as datas;
